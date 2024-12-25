67f5db4c362e667a6d8dd50d2eed644e46fb275f
prakash <prakashpander704@gmail.com>
2024-12-25 11:22:22 +0530

    bg change

---
+++ script.js
@@ -0,0 +1,30 @@
+let green = document.querySelector(".green");
+let yello = document.querySelector(".yello");
+let red = document.querySelector(".red");
+let blue = document.querySelector(".blue");
+let pink = document.querySelector(".pink");
+let orange = document.querySelector(".orange");
+
+green.addEventListener("click",() => {
+    document.body.style.backgroundColor = "green";
+})
+
+yello.addEventListener("click",() => {
+    document.body.style.backgroundColor = "yellow";
+})
+
+red.addEventListener("click",() => {
+    document.body.style.backgroundColor = "red";
+})
+
+blue.addEventListener("click",() => {
+    document.body.style.backgroundColor = "blue";
+})
+
+pink.addEventListener("click",() => {
+    document.body.style.backgroundColor = "pink";
+})
+
+orange.addEventListener("click",() => {
+    document.body.style.backgroundColor = "orange";
+})
